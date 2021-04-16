a5a4fdda80753aa8b59ea86b7a1387d4383e41b3
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-04-16 15:05:48 -0300

    aumentando tamanho dos campos de texto

--- blog/models.py
+++ blog/models.py
@@ -5,13 +5,13 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=50)    
+    title = models.CharField(max_length=60)    
     content = models.TextField()    
     last_edited_at = models.DateTimeField(auto_now=True)
     published = models.BooleanField(default=False)
     featured = models.BooleanField(default=False)
     banner_link = models.URLField()
-    font_banner = models.CharField(max_length=30)
+    font_banner = models.CharField(max_length=60)
     slug = models.CharField(max_length=55)
     
     def __str__(self) -> str:
