29d416c0c28152660df72ab2910b86d41b0c74ab
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-04-21 16:23:10 -0300

    aumentando número de listagem por página

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
 
 class SetPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 9
     page_size_query_param = 'page_size'
     max_page_size = 20
                
